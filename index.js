ec74d9ed9872abd98de8660cc73a3a8d866a195d
Malcolm Stone <67117350+Malcolmston@users.noreply.github.com>
2022-11-01 01:23:16 +0000

    Fixes the SQL server

--- index.js
+++ index.js
@@ -1,69 +1,94 @@
 var sqlite3 = require('sqlite3').verbose();
-	var db = new sqlite3.Database('uses.sqlite');
+var db = new sqlite3.Database('uses.sqlite');
 var Promise = require('promise');
 
-function addNewuser(fname,lname,username,password){
-		    return new Promise((resolve, reject) => {
-db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS  users  (person_id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL, last_name TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL)");
+function addNewuser(fname, lname, username, password) {
+	return new Promise((resolve, reject) => {
+		db.serialize(function() {
+			db.run("CREATE TABLE IF NOT EXISTS  users  (person_id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL, last_name TEXT NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL)");
 
 
-	
-  var stmt = db.prepare("INSERT INTO users VALUES (?,?,?,?,?)");
 
-  stmt.run(null,fname,lname,username,password);
-  
-  stmt.finalize();
+			var stmt = db.prepare("INSERT INTO users VALUES (?,?,?,?,?)");
 
-	db.all("SELECT * FROM users", [], (err, rows) => {
+			stmt.run(null, fname, lname, username, password);
 
-	
-          resolve( rows )
-        
-		
+			stmt.finalize();
 
-});
+			db.all("SELECT * FROM users", [], (err, rows) => {
+				resolve(err || rows)
+
+			});
 		})
-				
 
-	/*
-  db.each("SELECT * FROM users", function(err, row) {
-      console.log(row);
-  });
-*/
-});
+	});
 
 }
 
-function getNewlogIn(username,password){
-	    return new Promise((resolve, reject) => {
+function getNewlogIn(username, password) {
+	return new Promise((resolve, reject) => {
 
-			
-	db.all("SELECT * FROM users", [], (err, rows) => {
 
-		 if (err) {
-          reject(err)
-        } else {
-          resolve( rows.filter(function(x) {
-		return x['username'] == username  && x['password'] == password
-	}) )
-        }
-		
+		db.all("SELECT * FROM users", [], (err, rows) => {
 
-});
-		})
+			if (err) {
+				reject(err)
+			} else {
+				console.log(rows.filter(function(x) {
+					return x['username'] == username && x['password'] == password
+				}))
+
+				resolve(rows.filter(function(x) {
+					return x['username'] == username && x['password'] == password
+				}))
+
+				/*
+			 resolve( rows.filter(function(x) {
+		   return x['username'] == username  && x['password'] == password
+	   }) )
+	*/
+			}
+
+
+		});
+	})
 }
 
-	/*
+
+function validate(username, password) {
+	return new Promise((resolve, reject) => {
+
+
+		db.all("SELECT * FROM users", [], (err, rows) => {
+
+			if (err) {
+				reject(err)
+			} else {
+				resolve( rows.filter(function(x) {
+					return x['username'] == username && x['password'] == password
+				}).length > 0  )
+
+				/*
+			 resolve( rows.filter(function(x) {
+		   return x['username'] == username  && x['password'] == password
+	   }) )
+	*/
+			}
+
+
+		});
+	})
+}
+/*
 	
-	db.all("SELECT * FROM users", [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  rows.forEach((row) => {
-    console.log(row);
-  });
-		
+db.all("SELECT * FROM users", [], (err, rows) => {
+if (err) {
+throw err;
+}
+rows.forEach((row) => {
+console.log(row);
+});
+	
 });
 
 */
@@ -90,41 +115,42 @@ const fetch = require('node-fetch')
 
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-    
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/login/main.html');
+	res.sendFile(__dirname + '/login/main.html');
 });
-    
+
+
 app.post('/login', urlencodedParser, (req, res) => {
-let  {Fame, Lame, ussername, pswd} = req.body
+	let { Fame, Lame, ussername, pswd } = req.body
 
-getNewlogIn(ussername,pswd ).then(function(x) {
-}).catch(function(x) {
-})
+	getNewlogIn(ussername, pswd).then(console.log)
 
-	
+
 });
+
 
 app.post('/signup', urlencodedParser, (req, res) => {
 
-	let  {Fame, Lame, ussername, pswd} = req.body
-getNewlogIn(ussername,pswd ).then(function(x) {
-if( !x ){
-addNewuser(Fame,Lame,ussername,pswd).then(console.log)
-}else{
-	
-}
+
+	let { Fame, Lame, ussername, pswd } = req.body
+
+	validate(ussername,pswd).then(function (p ) {
+		if(!p){
+				
+addNewuser(Fame,Lame,ussername,pswd).then(function(p) {
+	console.log(p )
+})
+		}
+	})
+
+
+
 });
-	
-//console.log(adr)
-
-//open(adr);
 
 
-});
-
-var server = app.listen(8080, function () {
-    console.log('Server is listening at port 5000...');
+var server = app.listen(8080, function() {
+	console.log('Server is listening at port 5000...');
 	console.log('http://192.168.0.104/5000/')
 });
 
