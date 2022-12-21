1f31abb8ecabc7c432cd1d82099475bd50df5b5d
Malcolm Stone <67117350+Malcolmston@users.noreply.github.com>
2022-12-21 03:26:48 +0000

    created a function named revoveItem to remove array items

--- place.js
+++ place.js
@@ -3,6 +3,13 @@ const users = [];
 const rooms = []
 const members = []
 
+Array.prototype.revoveItem = function(i){
+    let array = this
+let index = this.indexOf(i);
+if (index > -1) { // only splice array when item is found
+array.splice(index, 1); // 2nd parameter means remove one item only
+}
 
+return array
 
-
+}
