9f0e8132aaf60165b0188958551f9356e5c25dea
Malcolm Stone <67117350+Malcolmston@users.noreply.github.com>
2022-12-21 03:27:52 +0000

    added the functions to join chat,  get user,  Get room, and get all users

--- place.js
+++ place.js
@@ -13,3 +13,27 @@ array.splice(index, 1); // 2nd parameter means remove one item only
 return array
 
 }
+
+// join chat
+function newUser(id, username, room) {
+	const user = { id, username, room };
+
+	users.push(user);
+
+	return user;
+}
+
+// get user
+function getActiveUser(id) {
+	return users.find(user => user.id === id);
+}
+
+
+// Get room
+function getIndividualRoomUsers(room) {
+	return users.filter(user => user.room === room);
+}
+
+function getAllusers() {
+	return users.map(x => x.username)
+}
