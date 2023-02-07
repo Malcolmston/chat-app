372835515273fd79ea9a13eb2eec00c405f16833
Malcolmston <67117350+Malcolmston@users.noreply.github.com>
2023-02-07 16:06:37 +0000

    remove Cookies in html

--- js/login.js
+++ js/login.js
@@ -3,38 +3,7 @@
     return value || false; //(value === true && value !== false) && (value != undefined && value != null)
   }
 
-  // sets the user in localStorage
-  function setUser(user, psw) {
-    Cookies.set('username', user, { expires: 0.0000004, path: '' })
-    Cookies.set('password', psw, { expires: 0.0000004, path: '' })
 
-    //localStorage.setItem("username", user);
-    //localStorage.setItem("password", psw);
-  }
-
-  // removes the user in localStorage
-  function removeUser() {
-    Cookies.remove('username')
-    Cookies.remove('password')
-
-
-    //localStorage.removeItem("username");
-    //localStorage.removeItem("password");
-  }
-  // gets if user is true
-  function getUser() {
-    return isTrue(Cookies.get('username')) &&
-      isTrue(Cookies.get('username'))
-      ? [Cookies.get('username'), Cookies.get("password")]
-      : false;
-
-/*
-    return isTrue(localStorage.getItem("username")) &&
-      isTrue(localStorage.getItem("password"))
-      ? [localStorage.getItem("username"), localStorage.getItem("password")]
-      : false;
-      */
-  }
 
   const logIn = document.querySelector("#login");
   const signUp = document.querySelector("#signup");
@@ -81,25 +50,7 @@
 
   document.querySelectorAll("[type='submit']").forEach((x) => {
     x.addEventListener("click", function () {
-      if (this.parentElement.id == "login") {
-        setUser(
-          document.querySelector("#ussername").value,
-          document.querySelector("#pwd").value
-        );
-      }
-
-      if (this.parentElement.id == "signup") {
-        setUser(
-          document.querySelector("#ussername1").value,
-          document.querySelector("#pwd1").value
-        );
-      }
+     
     });
   });
 
-  if (getUser()) {
-    document.querySelector("#ussername").value = getUser()[0];
-    document.querySelector("#pwd").value = getUser()[1];
-
-    document.querySelector("#login").submit();
-  }
